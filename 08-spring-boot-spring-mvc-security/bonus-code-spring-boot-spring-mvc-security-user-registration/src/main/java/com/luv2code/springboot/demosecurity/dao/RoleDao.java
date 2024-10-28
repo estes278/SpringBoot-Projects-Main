5bf33beec7c029fa9695180173b74cde3a2f81fa
estes278 <estes278@gmail.com>
2024-10-28 15:37:40 -0400

    Brining all projects into one main space

--- 08-spring-boot-spring-mvc-security/bonus-code-spring-boot-spring-mvc-security-user-registration/src/main/java/com/luv2code/springboot/demosecurity/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.luv2code.springboot.demosecurity.dao;
-
-import com.luv2code.springboot.demosecurity.entity.Role;
-
-public interface RoleDao {
-
-	public Role findRoleByName(String theRoleName);
-	
-}
